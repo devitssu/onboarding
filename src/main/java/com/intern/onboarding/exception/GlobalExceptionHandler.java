3602d9a9178a08c19975d8cd26f9e3dc9ba619c4
itssu <63135789+devitssu@users.noreply.github.com>
2024-09-04 14:20:01 +0900

    ✨ 예외 처리를 위한 기본 구조 (#2)
    * ✨ CustomException 생성
    
    * ✨ GlobalExceptionHandler 생성

---
+++ src/main/java/com/intern/onboarding/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.intern.onboarding.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> customExceptionHandler(CustomException e) {
+        int statusCode = e.getStatusCode();
+        ErrorResponse body = ErrorResponse.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity
+                .status(statusCode)
+                .body(body);
+    }
+}
