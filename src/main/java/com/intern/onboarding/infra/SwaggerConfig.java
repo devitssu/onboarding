5d2a40309c5ad78f96671d4e0815510ffd904a0a
itssu <63135789+devitssu@users.noreply.github.com>
2024-09-05 17:01:41 +0900

    🔧 SwaggerConfig 추가 (#7)

---
+++ src/main/java/com/intern/onboarding/infra/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.intern.onboarding.infra;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("한달인턴 온보딩 과제 API")
+                        .version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+    }
+}
